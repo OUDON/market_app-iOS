8a46a79477ace36525f87181999963daa377d018
Takahiro Tomita <takahiro-tomita@cookpad.com>
2016-08-16 11:29:46 +0900

    Setup Recommend Item Cell

---
+++ Market/View/RecommendItemCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class RecommendItemCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var desctiptionLabel: UILabel!
+
+}
