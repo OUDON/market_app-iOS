621d844ab7edfd01b70874d32b672d9d8b591898
Takahiro Tomita <takahiro-tomita@cookpad.com>
2016-08-16 13:27:20 +0900

    Add Frameworks

--- Market/View/RecommendItemCell.swift
+++ Market/View/RecommendItemCell.swift
@@ -3,6 +3,12 @@ import UIKit
 class RecommendItemCell: UITableViewCell {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var desctiptionLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
 
+    func update(withItem item: Item) {
+        nameLabel.text = item.name
+        priceLabel.text = "\(item.price)円"
+        descriptionLabel.text = item.desc
+    }
 }
